5e17deb9a276754949de553ddbd68b9306ce4c17
AhmedAlMadani <ahmedmoeen87@gmail.com>
2022-11-13 02:58:47 +0200

    database creation complete

--- databaseGenerator/generate_database.py
+++ databaseGenerator/generate_database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def init_database():
-    with sqlite3.connect("password_vault.db") as db:
+    with sqlite3.connect("password_storage.db") as db:
         cursor = db.cursor()
     cursor.execute("""
             CREATE TABLE IF NOT EXISTS master(
