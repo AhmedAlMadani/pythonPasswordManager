d1cda070ca0b893c5dff5d13f4ffcd7056b11fe0
AhmedAlMadani <ahmedmoeen87@gmail.com>
2022-11-13 03:29:29 +0200

    update storage methods

--- storage/storage_methods.py
+++ storage/storage_methods.py
@@ -11,7 +11,7 @@ class StorageMethods:
         answer = simpledialog.askstring("Enter details", heading)
         return answer
 
-    def add_password(self, popup_screen):
+    def add_password(self, add_password_screen):
         platform = self.popup_entry("Platform")
         userid = self.popup_entry("Username/Email")
         password = self.popup_entry("Password")
@@ -19,16 +19,16 @@ class StorageMethods:
         insert_cmd = """INSERT INTO vault(platform, userid, password) VALUES (?, ?, ?)"""
         self.cursor.execute(insert_cmd, (platform, userid, password))
         self.db.commit()
-        popup_screen()
+        add_password_screen()
 
-    def update_password(self, id, popup_screen):
+    def update_password(self, id, update_password_screen):
         password = self.popup_entry("Enter New Password")
         self.cursor.execute(
             "UPDATE vault SET password = ? WHERE id = ?", (password, id))
         self.db.commit()
-        popup_screen()
+        update_password_screen()
 
-    def remove_password(self, id, popup_screen):
+    def remove_password(self, id, remove_password_screen):
         self.cursor.execute("DELETE FROM vault WHERE id = ?", (id,))
         self.db.commit()
-        popup_screen()
+        remove_password_screen()
